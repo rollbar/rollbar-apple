b5cd40c7ef95130abad099c4179f470a6ebc8980
Andrey Kornich <akornich@gmail.com>
2020-11-11 22:20:15 -0800

    added RollbarKSCrashTests.m

---
+++ RollbarKSCrash/Tests/RollbarKSCrashTests-ObjC/RollbarKSCrashTests.m
@@ -0,0 +1,16 @@
+//  Copyright (c) 2018 Rollbar, Inc. All rights reserved.
+
+#import <XCTest/XCTest.h>
+
+@import RollbarKSCrash;
+
+@interface RollbarKSCrashTests : XCTestCase
+@end
+
+@implementation RollbarKSCrashTests
+
+- (void)testBasics {
+}
+
+@end
+
