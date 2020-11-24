7103a6363d477e0063da811da7b13c50feee4add
Andrey Kornich <akornich@gmail.com>
2020-11-24 15:05:16 -0800

    chore: ref #9 - Basic CI implementation for master

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
         .macOS(.v10_10),
         .iOS(.v9),
         .tvOS(.v11),
-//        .watchOS(.v4),
+        .watchOS(.v4),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RollbarCommon",
             targets: ["RollbarCommon"]),
-        .library(
-            name: "RollbarDeploys",
-            targets: ["RollbarDeploys"]),
-        .library(
-            name: "RollbarNotifier",
-            targets: ["RollbarNotifier"]),
+        // .library(
+        //     name: "RollbarDeploys",
+        //     targets: ["RollbarDeploys"]),
+        // .library(
+        //     name: "RollbarNotifier",
+        //     targets: ["RollbarNotifier"]),
     ],
 //    dependencies: [
 //        // Dependencies declare other packages that this package depends on.
@@ -36,12 +36,11 @@ let package = Package(
 
         .target(
             name: "RollbarCommon",
-//            dependencies: ["RollbarCommon",],
+            dependencies: [],
             path: "RollbarCommon/Sources/RollbarCommon",
             publicHeadersPath: "include",
             cSettings: [
                 .headerSearchPath("RollbarCommon/Sources/RollbarCommon/**"),
-//                .define("DEFINES_MODULE"),
             ]
         ),
         .testTarget(
@@ -50,77 +49,75 @@ let package = Package(
             path: "RollbarCommon/Tests/RollbarCommonTests",
             cSettings: [
                 .headerSearchPath("RollbarCommon/Tests/RollbarCommonTests/**"),
-//                .define("DEFINES_MODULE"),
             ]
         ),
-//        .testTarget(
-//            name: "RollbarCommonTests-ObjC",
-//            dependencies: ["RollbarCommon"],
-//            path: "RollbarCommon/Tests/RollbarCommonTests-ObjC",
-//            cSettings: [
-//                .headerSearchPath("RollbarCommon/Tests/RollbarCommonTests-ObjC/**"),
-////                .define("DEFINES_MODULE"),
-//            ]
-//        ),
+       .testTarget(
+           name: "RollbarCommonTests-ObjC",
+           dependencies: ["RollbarCommon"],
+           path: "RollbarCommon/Tests/RollbarCommonTests-ObjC",
+           cSettings: [
+               .headerSearchPath("RollbarCommon/Tests/RollbarCommonTests-ObjC/**"),
+           ]
+       ),
 
 
-        .target(
-            name: "RollbarDeploys",
-            dependencies: ["RollbarCommon",],
-            path: "RollbarDeploys/Sources/RollbarDeploys",
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("RollbarDeploys/Sources/RollbarDeploys/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
-        .testTarget(
-            name: "RollbarDeploysTests",
-            dependencies: ["RollbarDeploys"],
-            path: "RollbarDeploys/Sources/RollbarDeploysTests",
-            cSettings: [
-                .headerSearchPath("RollbarDeploys/Tests/RollbarDeploysTests/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
-        .testTarget(
-            name: "RollbarDeploysTests-ObjC",
-            dependencies: ["RollbarDeploys"],
-            path: "RollbarDeploys/Sources/RollbarDeploysTests-ObjC",
-            cSettings: [
-                .headerSearchPath("RollbarDeploys/Tests/RollbarDeploysTests-ObjC/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
+        // .target(
+        //     name: "RollbarDeploys",
+        //     dependencies: ["RollbarCommon",],
+        //     path: "RollbarDeploys/Sources/RollbarDeploys",
+        //     publicHeadersPath: "include",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarDeploys/Sources/RollbarDeploys/**"),
+        //     ]
+        // ),
+        // .testTarget(
+        //     name: "RollbarDeploysTests",
+        //     dependencies: ["RollbarDeploys"],
+        //     path: "RollbarDeploys/Sources/RollbarDeploysTests",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarDeploys/Tests/RollbarDeploysTests/**"),
+        //     ]
+        // ),
+        // .testTarget(
+        //     name: "RollbarDeploysTests-ObjC",
+        //     dependencies: ["RollbarDeploys"],
+        //     path: "RollbarDeploys/Sources/RollbarDeploysTests-ObjC",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarDeploys/Tests/RollbarDeploysTests-ObjC/**"),
+        //     ]
+        // ),
 
 
-        .target(
-            name: "RollbarNotifier",
-            dependencies: ["RollbarCommon",],
-            path: "RollbarNotifier/Sources/RollbarNotifier",
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("RollbarNotifier/Sources/RollbarNotifier/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
-        .testTarget(
-            name: "RollbarNotifierTests",
-            dependencies: ["RollbarNotifier"],
-            path: "RollbarNotifier/Sources/RollbarNotifierTests",
-            cSettings: [
-                .headerSearchPath("RollbarNotifier/Tests/RollbarNotifierTests/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
-        .testTarget(
-            name: "RollbarNotifierTests-ObjC",
-            dependencies: ["RollbarNotifier"],
-            path: "RollbarNotifier/Sources/RollbarNotifierTests-ObjC",
-            cSettings: [
-                .headerSearchPath("RollbarNotifier/Tests/RollbarNotifierTests-ObjC/**"),
-//                .define("DEFINES_MODULE"),
-            ]
-        ),
+        // .target(
+        //     name: "RollbarNotifier",
+        //     dependencies: ["RollbarCommon",],
+        //     path: "RollbarNotifier/Sources/RollbarNotifier",
+        //     publicHeadersPath: "include",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarNotifier/Sources/RollbarNotifier/**"),
+        //     ]
+        // ),
+        // .testTarget(
+        //     name: "RollbarNotifierTests",
+        //     dependencies: ["RollbarNotifier"],
+        //     path: "RollbarNotifier/Sources/RollbarNotifierTests",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarNotifier/Tests/RollbarNotifierTests/**"),
+        //     ]
+        // ),
+        // .testTarget(
+        //     name: "RollbarNotifierTests-ObjC",
+        //     dependencies: ["RollbarNotifier"],
+        //     path: "RollbarNotifier/Sources/RollbarNotifierTests-ObjC",
+        //     cSettings: [
+        //         .headerSearchPath("RollbarNotifier/Tests/RollbarNotifierTests-ObjC/**"),
+        //     ]
+        // ),
+
+    ],
+    swiftLanguageVersions: [
+        SwiftVersion.v4,
+        SwiftVersion.v4_2,
+        SwiftVersion.v5,
     ]
 )
