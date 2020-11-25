b5df688603dfe004ee436398ba08eafdc0c47385
Andrey Kornich <akornich@gmail.com>
2020-11-24 20:22:05 -0800

    chore: ref #9

--- RollbarKSCrash/Tests/RollbarKSCrashTests/RollbarKSCrashTests.swift
+++ RollbarKSCrash/Tests/RollbarKSCrashTests/RollbarKSCrashTests.swift
@@ -6,7 +6,7 @@ final class RollbarKSCrashTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RollbarKSCrash().text, "Hello, World!")
+        //XCTAssertEqual(RollbarKSCrash().text, "Hello, World!")
     }
 
     static var allTests = [
