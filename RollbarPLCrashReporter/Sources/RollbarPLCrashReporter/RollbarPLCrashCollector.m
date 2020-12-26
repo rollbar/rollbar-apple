3efd0a9e2ceedbf8e40f8804be3686f4123f4c67
Andrey Kornich <akornich@gmail.com>
2020-12-26 00:05:33 -0800

    first cut of PLCrashReporter integration

--- RollbarPLCrashReporter/Sources/RollbarPLCrashReporter/RollbarPLCrashCollector.m
+++ RollbarPLCrashReporter/Sources/RollbarPLCrashReporter/RollbarPLCrashCollector.m
@@ -4,6 +4,7 @@
 //
 //  Created by Andrey Kornich on 2020-12-21.
 //
+@import CrashReporter;
 
 #import "RollbarPLCrashCollector.h"
 #import "RollbarCrashReportData.h"
@@ -12,7 +13,7 @@
 
 @implementation RollbarPLCrashCollector
 
-+ (void)initialize {
++(void)initialize {
     
     if (self == [RollbarPLCrashCollector class]) {
         //RollbarKSCrashInstallation *installation = [RollbarKSCrashInstallation sharedInstance];
@@ -20,25 +21,73 @@
     }
 }
 
-- (void)collectCrashReportsWithObserver:(NSObject<RollbarCrashCollectorObserver> *)observer {
+-(void)collectCrashReportsWithObserver:(NSObject<RollbarCrashCollectorObserver> *)observer {
+    
+    // Configure our reporter:
+    PLCrashReporterSignalHandlerType signalHandlerType =
+#if !TARGET_OS_TV
+        PLCrashReporterSignalHandlerTypeMach;
+#else
+        PLCrashReporterSignalHandlerTypeBSD;
+#endif
+    
+    PLCrashReporterConfig *config =
+    [[PLCrashReporterConfig alloc] initWithSignalHandlerType: signalHandlerType
+                                       symbolicationStrategy: PLCrashReporterSymbolicationStrategyAll
+    ];
+    PLCrashReporter *reporter =
+    [[PLCrashReporter alloc] initWithConfiguration: config];
+
+    
+    if (!reporter || ![reporter hasPendingCrashReport]) {
+        return;
+    }
+
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSError *error;
+    
+    NSArray *paths =
+    NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    if (![fm createDirectoryAtPath: documentsDirectory
+       withIntermediateDirectories: YES
+                        attributes:nil
+                             error: &error]) {
+        NSLog(@"Could not create documents directory: %@", error);
+        return;
+    }
+
+    NSData *data =
+    [reporter loadPendingCrashReportDataAndReturnError: &error];
+    if (data == nil) {
+        NSLog(@"Failed to load crash report data: %@", error);
+        return;
+    }
+    [reporter purgePendingCrashReport];
+
+    PLCrashReport *report =
+    [[PLCrashReport alloc] initWithData: data
+                                  error: &error];
+    if (report == nil) {
+       NSLog(@"Failed to parse crash report: %@", error);
+       return;
+    }
+    
+    NSString *crashReport =
+    [PLCrashReportTextFormatter stringValueForCrashReport: report
+                                           withTextFormat: PLCrashReportTextFormatiOS];
+    NSLog(@"%@", crashReport);
+
+    if (!crashReport) {
+        return;
+    }
     
     NSMutableArray<RollbarCrashReportData *> *crashReports = [[NSMutableArray alloc] init];
+    
+    RollbarCrashReportData *crashReportData =
+    [[RollbarCrashReportData alloc] initWithCrashReport:crashReport];
 
-//    RollbarKSCrashInstallation *installation = [RollbarKSCrashInstallation sharedInstance];
-//    [installation sendAllReportsWithCompletion:^(NSArray *filteredReports, BOOL completed, NSError *error) {
-//        if (error) {
-//            RollbarSdkLog(@"Could not enable crash reporter: %@", [error localizedDescription]);
-//        } else if (completed) {
-//            //[notifier processSavedItems];
-//            for (NSString *crashReport in filteredReports) {
-//                
-//                RollbarCrashReportData *crashReportData =
-//                [[RollbarCrashReportData alloc] initWithCrashReport:crashReport];
-//                
-//                [crashReports addObject:crashReportData];
-//            }
-//        }
-//    }];
+    [crashReports addObject:crashReportData];
 
     [observer onCrashReportsCollectionCompletion:crashReports];
 }
