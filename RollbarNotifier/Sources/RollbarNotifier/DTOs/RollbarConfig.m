3f8cecfc9a15ac889116e98be9562ac1a02f213d
Andrey Kornich <akornich@gmail.com>
2021-02-16 19:46:53 -0800

    fixing Cocoapods specs

--- RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarConfig.m
+++ RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarConfig.m
@@ -22,7 +22,7 @@
 
 #pragma mark - constants
 
-static NSString * const NOTIFIER_VERSION = @"2.0.0-alpha34";
+static NSString * const NOTIFIER_VERSION = @"2.0.0-alpha35";
 
 static NSString * const NOTIFIER_NAME = @"rollbar-apple";
 
