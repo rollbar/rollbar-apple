0df5f62b36187b38b61a0a5ab9fcb9c49e8574ea
Andrey Kornich <akornich@gmail.com>
2021-03-11 10:12:17 -0800

    added macosAppSwift demo app

---
+++ Demos/macosAppSwift/macosAppSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macosAppSwift
+//
+//  Created by Andrey Kornich on 2021-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
