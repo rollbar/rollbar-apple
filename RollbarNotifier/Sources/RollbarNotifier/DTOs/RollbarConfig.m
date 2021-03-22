af662bd8f80d296ed060b12b48b86dbf83291dd7
Andrey Kornich <akornich@gmail.com>
2021-03-22 16:37:05 -0700

    - fix: RollbarPLCrashReporter podspec

--- RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarConfig.m
+++ RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarConfig.m
@@ -22,7 +22,7 @@
 
 #pragma mark - constants
 
-static NSString * const NOTIFIER_VERSION = @"2.0.0-beta8";
+static NSString * const NOTIFIER_VERSION = @"2.0.0-beta9";
 
 static NSString * const NOTIFIER_NAME = @"rollbar-apple";
 
