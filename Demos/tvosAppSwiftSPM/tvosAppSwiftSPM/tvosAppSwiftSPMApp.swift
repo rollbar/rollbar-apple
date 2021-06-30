09ecb680777d3a57f0e96c846541cea948363a9e
Andrey Kornich <akornich@gmail.com>
2021-06-30 15:28:08 -0700

    tvosAppSwiftSPM: initial project

---
+++ Demos/tvosAppSwiftSPM/tvosAppSwiftSPM/tvosAppSwiftSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvosAppSwiftSPMApp.swift
+//  tvosAppSwiftSPM
+//
+//  Created by Andrey Kornich on 2021-06-30.
+//
+
+import SwiftUI
+
+@main
+struct tvosAppSwiftSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
