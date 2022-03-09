77440162315e6841e6b9a58da91d7a8b7bab30bb
Andrey Kornich <akornich@gmail.com>
2022-03-08 22:09:42 -0800

    test: ref #134

--- RollbarCocoaLumberjack/Tests/RollbarCocoaLumberjackTests-ObjC/RollbarCocoaLumberjackTests.m
+++ RollbarCocoaLumberjack/Tests/RollbarCocoaLumberjackTests-ObjC/RollbarCocoaLumberjackTests.m
@@ -1,7 +1,6 @@
 //  Copyright (c) 2018 Rollbar, Inc. All rights reserved.
 
 #import <XCTest/XCTest.h>
-//#import "../../../UnitTests/RollbarUnitTestSettings.h"
 #import "RollbarTestUtil.h"
 
 #define LOG_LEVEL_DEF ddLogLevel
