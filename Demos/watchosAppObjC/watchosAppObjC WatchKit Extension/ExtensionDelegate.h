9b384352534751a3fa8e6e36080a8d16c6a6a226
Andrey Kornich <akornich@gmail.com>
2022-04-26 16:53:36 -0700

    test: ref #168

---
+++ Demos/watchosAppObjC/watchosAppObjC WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,12 @@
+//
+//  ExtensionDelegate.h
+//  watchosAppObjC WatchKit Extension
+//
+//  Created by Andrey Kornich on 2022-04-26.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
