2a453932a74c08204433b9ac018d533399795152
Andrey Kornich <akornich@gmail.com>
2022-04-26 16:51:17 -0700

    test: ref #167

---
+++ Demos/tvosAppObjC/tvosAppObjC/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  tvosAppObjC
+//
+//  Created by Andrey Kornich on 2022-04-26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
