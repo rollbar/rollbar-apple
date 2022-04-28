e5b8cb26c898139a1faa720666ea9f77e9dbd0c5
Andrey Kornich <akornich@gmail.com>
2022-04-28 16:24:42 -0700

    feat: ref #165

--- RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarSessionState.m
+++ RollbarNotifier/Sources/RollbarNotifier/DTOs/RollbarSessionState.m
@@ -13,8 +13,13 @@
 
 #pragma mark - data field keys
 
+
 static NSString * const DFK_OS_VERSION = @"os_version";
 static NSString * const DFK_APP_VERSION = @"app_version";
+
+static NSString * const DFK_APP_ID = @"app_id";
+static NSString * const DFK_SESSION_ID = @"session_id";
+
 static NSString * const DFK_SESSION_TIMESTAMP = @"session_timestamp";
 static NSString * const DFK_APP_MEMORY_WARNING_TIMESTAMP = @"app_memory_warning_timestamp";
 static NSString * const DFK_APP_TERMINATION_TIMESTAMP = @"app_termination_timestamp";
@@ -45,6 +50,36 @@ static NSString * const DFK_APP_IN_BACKGROUND_FLAG = @"app_in_background";
 - (void)setAppVersion:(NSString *)value {
     
     [self setData:value byKey:DFK_APP_VERSION];
+}
+
+
+-(nullable NSUUID *)appID {
+    
+    NSString *result = [self getDataByKey:DFK_APP_ID];
+    if (result) {
+        
+        return [[NSUUID alloc] initWithUUIDString:result];
+    }
+    return nil;
+}
+
+-(void)setAppID:(nullable NSUUID *)value {
+    [self setData:value.UUIDString byKey:DFK_APP_ID];
+}
+
+
+-(nullable NSUUID *)sessionID {
+    
+    NSString *result = [self getDataByKey:DFK_SESSION_ID];
+    if (result) {
+        
+        return [[NSUUID alloc] initWithUUIDString:result];
+    }
+    return nil;
+}
+
+-(void)setSessionID:(nullable NSUUID *)value {
+    [self setData:value.UUIDString byKey:DFK_SESSION_ID];
 }
 
 
--- RollbarNotifier/Sources/RollbarNotifier/include/RollbarSessionState.h
+++ RollbarNotifier/Sources/RollbarNotifier/include/RollbarSessionState.h
@@ -19,6 +19,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy, nonnull) NSString *osVersion;
 @property (nonatomic, copy, nonnull) NSString *appVersion;
 
+@property (atomic, nullable) NSUUID *appID;
+@property (atomic, nullable) NSUUID *sessionID;
+
 @property (nonatomic, copy, nonnull) NSDate *sessionStartTimestamp;
 @property (nonatomic, copy, nonnull) NSDate *appMemoryWarningTimestamp;
 @property (nonatomic, copy, nonnull) NSDate *appTerminationTimestamp;
