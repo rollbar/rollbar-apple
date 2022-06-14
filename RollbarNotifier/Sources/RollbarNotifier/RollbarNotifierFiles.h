fe686d660907c02b9c16d18d35c1624e225934ba
Andrey Kornich <akornich@gmail.com>
2022-06-13 19:01:53 -0700

    refactor: RollbarNotifierFiles

--- RollbarNotifier/Sources/RollbarNotifier/RollbarNotifierFiles.h
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarNotifierFiles.h
@@ -11,6 +11,29 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RollbarNotifierFiles : NSObject
 
+#pragma mark - notifier files
+
++ (nonnull NSString * const)itemsQueue;
++ (nonnull NSString * const)itemsQueueState;
+
++ (nonnull NSString * const)telemetryQueue;
+
++ (nonnull NSString * const)runtimeSession;
+
++ (nonnull NSString * const)appQuit;
+
++ (nonnull NSString * const)payoadsLog;
+
++ (nonnull NSString * const)config;
+
+#pragma mark - static utility nature
+
++ (instancetype)new NS_UNAVAILABLE;
++ (instancetype)allocWithZone:(struct _NSZone *)zone NS_UNAVAILABLE;
++ (instancetype)alloc NS_UNAVAILABLE;
++ (id)copyWithZone:(struct _NSZone *)zone NS_UNAVAILABLE;
++ (id)mutableCopyWithZone:(struct _NSZone *)zone NS_UNAVAILABLE;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- RollbarNotifier/Sources/RollbarNotifier/RollbarNotifierFiles.m
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarNotifierFiles.m
@@ -1,12 +1,53 @@
-//
-//  RollbarNotifierFiles.m
-//  
-//
-//  Created by Andrey Kornich on 2022-06-06.
-//
-
 #import "RollbarNotifierFiles.h"
 
+static NSString * const PAYLOADS_FILE_NAME = @"rollbar.payloads";
+
+static NSString * const QUEUED_ITEMS_FILE_NAME = @"rollbar.items";
+static NSString * const QUEUED_ITEMS_STATE_FILE_NAME = @"rollbar.state";
+
+static NSString * const QUEUED_TELEMETRY_ITEMS_FILE_NAME = @"rollbar.telemetry";
+
+static NSString * const SESSION_FILE_NAME = @"rollbar.session";
+static NSString * const APP_QUIT_FILE_NAME = @"rollbar.appquit";
+
+static NSString * const CONFIG_FILE_NAME = @"rollbar.config";
+
 @implementation RollbarNotifierFiles
+
++ (nonnull NSString * const)itemsQueue {
+    
+    return QUEUED_ITEMS_FILE_NAME;
+}
+
++ (nonnull NSString * const)itemsQueueState {
+    
+    return QUEUED_ITEMS_STATE_FILE_NAME;
+}
+
++ (nonnull NSString * const)telemetryQueue {
+    
+    return QUEUED_TELEMETRY_ITEMS_FILE_NAME;
+}
+
++ (nonnull NSString * const)runtimeSession {
+    
+    return SESSION_FILE_NAME;
+}
+
++ (nonnull NSString * const)appQuit {
+    
+    return APP_QUIT_FILE_NAME;
+}
+
++ (nonnull NSString * const)payoadsLog {
+    
+    return PAYLOADS_FILE_NAME;
+}
+
++ (nonnull NSString * const)config {
+    
+    return CONFIG_FILE_NAME;
+}
+
 
 @end
