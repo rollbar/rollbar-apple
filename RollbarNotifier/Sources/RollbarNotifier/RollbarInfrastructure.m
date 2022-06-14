d3fad44b6fd8bdfd3de9662a4234657cb58f61b7
Andrey Kornich <akornich@gmail.com>
2022-06-13 19:03:33 -0700

    refactor: RollbarInfrastructure

--- RollbarNotifier/Sources/RollbarNotifier/RollbarInfrastructure.m
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarInfrastructure.m
@@ -9,4 +9,26 @@
 
 @implementation RollbarInfrastructure
 
+- (nonnull instancetype)configureWith:(RollbarConfig *)rollbarConfig {
+    
+    self->_config = rollbarConfig;
+    
+    return self;
+}
+
+#pragma mark - Sigleton pattern
+
++ (nonnull instancetype)sharedInstance {
+    
+    static id singleton;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+        singleton = [self new];
+    });
+    
+    return singleton;
+}
+
 @end
