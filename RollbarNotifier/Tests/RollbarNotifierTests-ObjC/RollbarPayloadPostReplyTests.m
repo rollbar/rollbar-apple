c26026ddf94e472f3a4a8770e2d407bd16964471
Andrey Kornich <akornich@gmail.com>
2022-06-14 12:48:47 -0700

    test: RollbarPayloadPostReplyTests

--- RollbarNotifier/Tests/RollbarNotifierTests-ObjC/RollbarPayloadPostReplyTests.m
+++ RollbarNotifier/Tests/RollbarNotifierTests-ObjC/RollbarPayloadPostReplyTests.m
@@ -59,7 +59,7 @@
 }
 
 - (void)testPerformanceExample {
-    // This is an example of a performance test case.
+    
     [self measureBlock:^{
         RollbarPayloadPostReply *reply = [[RollbarPayloadPostReply alloc] initWithStatusCode:200
                                                                                    rateLimit:60
