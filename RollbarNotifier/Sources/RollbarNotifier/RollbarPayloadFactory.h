ed6603085bbe76f2df04f7cf74ebb831a37b5990
Andrey Kornich <akornich@gmail.com>
2022-06-14 22:32:13 -0700

    refactor: RollbarPayloadFactory

--- RollbarNotifier/Sources/RollbarNotifier/RollbarPayloadFactory.h
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarPayloadFactory.h
@@ -7,9 +7,37 @@
 
 #import <Foundation/Foundation.h>
 
+#import "RollbarLevel.h"
+
+@class RollbarConfig;
+@class RollbarPayload;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RollbarPayloadFactory : NSObject
+
+- (nullable RollbarPayload *)payloadWithLevel:(RollbarLevel)level
+                                  crashReport:(nonnull NSString *)crashReport;
+
+- (nullable RollbarPayload *)payloadWithLevel:(RollbarLevel)level
+                                      message:(nonnull NSString *)message
+                                         data:(nullable NSDictionary<NSString *, id> *)data
+                                      context:(nullable NSString *)context;
+
+- (nullable RollbarPayload *)payloadWithLevel:(RollbarLevel)level
+                                    exception:(nonnull NSException *)exception
+                                         data:(nullable NSDictionary<NSString *, id> *)data
+                                      context:(nullable NSString *)context;
+
+- (nullable RollbarPayload *)payloadWithLevel:(RollbarLevel)level
+                                        error:(nonnull NSError *)error
+                                         data:(nullable NSDictionary<NSString *, id> *)data
+                                      context:(nullable NSString *)context;
+
+- (instancetype)initWithConfig:(nonnull RollbarConfig *)config NS_DESIGNATED_INITIALIZER;
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)factoryWithConfig:(nonnull RollbarConfig *)config;
 
 @end
 
