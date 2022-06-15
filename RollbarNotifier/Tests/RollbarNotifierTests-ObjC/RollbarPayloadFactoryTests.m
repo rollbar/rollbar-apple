eb9a2f7e2e993c62c404ef10f40a04611dfd85e2
Andrey Kornich <akornich@gmail.com>
2022-06-15 12:36:04 -0700

    test: RollbarPayloadFactoryTests

---
+++ RollbarNotifier/Tests/RollbarNotifierTests-ObjC/RollbarPayloadFactoryTests.m
@@ -0,0 +1,36 @@
+//
+//  RollbarPayloadFactoryTests.m
+//  
+//
+//  Created by Andrey Kornich on 2022-06-15.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RollbarPayloadFactoryTests : XCTestCase
+
+@end
+
+@implementation RollbarPayloadFactoryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
