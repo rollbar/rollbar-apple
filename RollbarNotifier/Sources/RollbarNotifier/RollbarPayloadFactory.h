485e53101f6a53ebfd679e5073c23fbccb3f4dcc
Andrey Kornich <akornich@gmail.com>
2022-06-14 22:26:55 -0700

    refactor: added RollbarPayloadFactory

---
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarPayloadFactory.h
@@ -0,0 +1,16 @@
+//
+//  RollbarPayloadFactory.h
+//  
+//
+//  Created by Andrey Kornich on 2022-06-14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RollbarPayloadFactory : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarPayloadFactory.m
@@ -0,0 +1,12 @@
+//
+//  RollbarPayloadFactory.m
+//  
+//
+//  Created by Andrey Kornich on 2022-06-14.
+//
+
+#import "RollbarPayloadFactory.h"
+
+@implementation RollbarPayloadFactory
+
+@end
