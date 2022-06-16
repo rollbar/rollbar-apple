8fe442ae969bf86b0bf061ea0645941c2f627a93
Andrey Kornich <akornich@gmail.com>
2022-06-15 22:16:56 -0700

    test: added common test helpers

---
+++ RollbarNotifier/Tests/RollbarNotifierTests-ObjC/XCTestCase+RollbarNotifierTest.h
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+RollbarNotifierTest.h
+//  
+//
+//  Created by Andrey Kornich on 2022-06-15.
+//
+
+#import <XCTest/XCTest.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTestCase (RollbarNotifierTest)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RollbarNotifier/Tests/RollbarNotifierTests-ObjC/XCTestCase+RollbarNotifierTest.m
@@ -0,0 +1,12 @@
+//
+//  XCTestCase+RollbarNotifierTest.m
+//  
+//
+//  Created by Andrey Kornich on 2022-06-15.
+//
+
+#import "XCTestCase+RollbarNotifierTest.h"
+
+@implementation XCTestCase (RollbarNotifierTest)
+
+@end
