14a9b6ebe79dbcca529bc1cf8477e406e263d566
Andrey Kornich <akornich@gmail.com>
2022-06-23 23:22:18 -0700

    refactor: RollbarInfrastructure

--- RollbarNotifier/Sources/RollbarNotifier/RollbarInfrastructure.m
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarInfrastructure.m
@@ -7,11 +7,6 @@
     @private
     RollbarConfig *_configuration;
     RollbarLogger *_logger;
-    NSString *_oomDetectionFilePath;
-    NSString *_queuedItemsFilePath;
-    NSString *_stateFilePath;
-    NSString *_payloadsFilePath;
-    NSMutableDictionary *_queueState;
 }
 
 #pragma mark - Sigleton pattern
@@ -24,6 +19,10 @@
     dispatch_once(&onceToken, ^{
         
         singleton = [self new];
+
+        RollbarSdkLog(@"%@ singleton created!",
+                      [RollbarInfrastructure className]
+                      );
     });
     
     return singleton;
@@ -44,7 +43,10 @@
     self->_configuration = rollbarConfig;
     self->_logger = nil;
     
-    [self setupInternalStorage];
+    RollbarSdkLog(@"%@ is configured with this RollbarConfig instance: \n%@",
+                  [RollbarInfrastructure className],
+                  rollbarConfig
+                  );
     
     return self;
 }
@@ -100,63 +102,10 @@
     //TODO: complete full validation implementation...
 }
 
-- (void)setupInternalStorage {
-    
-    // create working cache directory:
-    [RollbarCachesDirectory ensureCachesDirectoryExists];
-    NSString *cachesDirectory = [RollbarCachesDirectory directory];
-    
-    // make sure we have all the data files set:
-    self->_queuedItemsFilePath =
-    [cachesDirectory stringByAppendingPathComponent:[RollbarNotifierFiles itemsQueue]];
-    self->_stateFilePath =
-    [cachesDirectory stringByAppendingPathComponent:[RollbarNotifierFiles itemsQueueState]];
-    
-    // create the queued items file if does not exist already:
-    if (![[NSFileManager defaultManager] fileExistsAtPath:self->_queuedItemsFilePath]) {
-        [[NSFileManager defaultManager] createFileAtPath:self->_queuedItemsFilePath
-                                                contents:nil
-                                              attributes:nil];
-    }
-    
-    // create state tracking file if does not exist already:
-    if ([[NSFileManager defaultManager] fileExistsAtPath:self->_stateFilePath]) {
-        NSData *stateData = [NSData dataWithContentsOfFile:self->_stateFilePath];
-        if (stateData) {
-            NSDictionary *state = [NSJSONSerialization JSONObjectWithData:stateData
-                                                                  options:0
-                                                                    error:nil];
-            self->_queueState = [state mutableCopy];
-        } else {
-            RollbarSdkLog(@"There was an error restoring saved queue state");
-        }
-    }
-    
-    // let's make sure we always recover into a good state if applicable:
-    if (!self->_queueState) {
-        self->_queueState = [@{
-            @"offset": [NSNumber numberWithUnsignedInt:0],
-            @"retry_count": [NSNumber numberWithUnsignedInt:0]
-        } mutableCopy];
-        [self saveQueueState];
-    }
-}
 
 - (void)configureInfrastructure {
     
     //TODO: implement...
-}
-
-- (void)saveQueueState {
-    NSError *error;
-    NSData *data = [NSJSONSerialization rollbar_dataWithJSONObject:self->_queueState
-                                                           options:0
-                                                             error:&error
-                                                              safe:true];
-    if (error) {
-        RollbarSdkLog(@"Error: %@", [error localizedDescription]);
-    }
-    [data writeToFile:self->_stateFilePath atomically:YES];
 }
 
 @end
