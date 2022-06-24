83bbae05fa884233ca2001b04727b60ce729d68f
Andrey Kornich <akornich@gmail.com>
2022-06-23 23:23:03 -0700

    refactor: RollbarThread

--- RollbarNotifier/Sources/RollbarNotifier/RollbarThread.m
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarThread.m
@@ -14,17 +14,10 @@
 #import "RollbarModule.h"
 #import "RollbarDestination.h"
 #import "RollbarProxy.h"
+#import "RollbarSender.h"
+#import "RollbarPayloadPostReply.h"
 
 static NSUInteger MAX_RETRY_COUNT = 5;
-
-/// Rollbar API Service enforced payload rate limit:
-static NSString * const RESPONSE_HEADER_RATE_LIMIT = @"x-rate-limit-limit";
-/// Rollbar API Service enforced remaining payload count until the limit is reached:
-static NSString * const RESPONSE_HEADER_REMAINING_COUNT = @"x-rate-limit-remaining";
-/// Rollbar API Service enforced rate limit reset time for the current limit window:
-static NSString * const RESPONSE_HEADER_RESET_TIME = @"x-rate-limit-reset";
-/// Rollbar API Service enforced rate limit remaining seconds of the current limit window:
-static NSString * const RESPONSE_HEADER_REMAINING_SECONDS = @"x-rate-limit-remaining-seconds";
 
 @implementation RollbarThread {
 
@@ -99,13 +92,6 @@ static NSString * const RESPONSE_HEADER_REMAINING_SECONDS = @"x-rate-limit-remai
     [cachesDirectory stringByAppendingPathComponent:[RollbarNotifierFiles itemsQueue]];
     self->_stateFilePath =
     [cachesDirectory stringByAppendingPathComponent:[RollbarNotifierFiles itemsQueueState]];
-//    payloadsFilePath =
-//    [cachesDirectory stringByAppendingPathComponent:[RollbarNotifierFiles payoadsLog]];
-    
-    // either create or overwrite the payloads log file:
-//    [[NSFileManager defaultManager] createFileAtPath:payloadsFilePath
-//                                            contents:nil
-//                                          attributes:nil];
     
     // create the queued items file if does not exist already:
     if (![[NSFileManager defaultManager] fileExistsAtPath:self->_queuedItemsFilePath]) {
@@ -118,26 +104,23 @@ static NSString * const RESPONSE_HEADER_REMAINING_SECONDS = @"x-rate-limit-remai
     if ([[NSFileManager defaultManager] fileExistsAtPath:self->_stateFilePath]) {
         NSData *stateData = [NSData dataWithContentsOfFile:self->_stateFilePath];
         if (stateData) {
-            
             NSDictionary *state = [NSJSONSerialization JSONObjectWithData:stateData
                                                                   options:0
                                                                     error:nil];
             self->_queueState = [state mutableCopy];
         } else {
-            
             RollbarSdkLog(@"There was an error restoring saved queue state");
         }
     }
+    
+    // let's make sure we always recover into a good state if applicable:
     if (!self->_queueState) {
-        
         self->_queueState = [@{
             @"offset": [NSNumber numberWithUnsignedInt:0],
             @"retry_count": [NSNumber numberWithUnsignedInt:0]
         } mutableCopy];
-        
         [self saveQueueState];
-    }
-}
+    }}
 
 - (void)saveQueueState {
 
@@ -440,208 +423,18 @@ static NSString * const RESPONSE_HEADER_REMAINING_SECONDS = @"x-rate-limit-remai
         return NO;
     }
     
-    NSURL *url = [NSURL URLWithString:config.destination.endpoint];
-    if (nil == url) {
-        return NO;
-    }
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    
-    [request setHTTPMethod:@"POST"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:config.destination.accessToken forHTTPHeaderField:@"X-Rollbar-Access-Token"];
-    [request setHTTPBody:payload];
-    
-    if (YES == config.developerOptions.logPayload) {
-        NSString *payloadString = [[NSString alloc]initWithData:payload
-                                                       encoding:NSUTF8StringEncoding
-        ];
-        NSLog(@"%@", payloadString);
-        //TODO: if config.developerOptions.logPayloadFile is defined, save the payload into the file...
+    RollbarPayloadPostReply *reply = [[RollbarSender new] sendPayload:payload usingConfig:config];
+    if (reply && (0 == reply.statusCode)) {
+        return YES;
     }
     
-    if (NO == config.developerOptions.transmit) {
-        return YES; // we just successfully short-circuit here...
-    }
-    
-    __block BOOL result = NO;
-    
-    // This requires iOS 7.0+
-    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    
-    if (config.httpProxy.enabled
-        || config.httpsProxy.enabled) {
-        
-        NSDictionary *connectionProxyDictionary =
-        @{
-            @"HTTPEnable"   : [NSNumber numberWithBool:config.httpProxy.enabled],
-            @"HTTPProxy"    : config.httpProxy.proxyUrl,
-            @"HTTPPort"     : [NSNumber numberWithUnsignedInteger:config.httpProxy.proxyPort],
-            @"HTTPSEnable"  : [NSNumber numberWithBool:config.httpsProxy.enabled],
-            @"HTTPSProxy"   : config.httpsProxy.proxyUrl,
-            @"HTTPSPort"    : [NSNumber numberWithUnsignedInteger:config.httpsProxy.proxyPort]
-        };
-        
-        NSURLSessionConfiguration *sessionConfig =
-        [NSURLSessionConfiguration ephemeralSessionConfiguration];
-        sessionConfig.connectionProxyDictionary = connectionProxyDictionary;
-        session = [NSURLSession sessionWithConfiguration:sessionConfig];
-    }
-    
-    NSURLSessionDataTask *dataTask =
-    [session dataTaskWithRequest:request
-               completionHandler:^(
-                                   NSData * _Nullable data,
-                                   NSURLResponse * _Nullable response,
-                                   NSError * _Nullable error) {
-                                       result = [self checkPayloadResponse:response
-                                                                     error:error
-                                                                      data:data];
-                                       dispatch_semaphore_signal(sem);
-                                   }];
-    [dataTask resume];
-    
-    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
-    
-    return result;
+    return NO;
 }
 
 /// This is a DEPRECATED method left for some backward compatibility for very old clients eventually moving to this more recent implementation.
 /// Use/maintain sendPayload:usingConfig: instead!
 - (BOOL)sendPayload:(NSData *)payload {
     
-    return NO;
-//    if ((nil == payload)
-//        || (nil == self.configuration)
-//        || (nil == self.configuration.destination)
-//        || (nil == self.configuration.destination.endpoint)
-//        || (nil == self.configuration.destination.accessToken)
-//        || (0 == self.configuration.destination.endpoint.length)
-//        || (0 == self.configuration.destination.accessToken.length)
-//        ) {
-//
-//        return NO;
-//    }
-//
-//    NSURL *url = [NSURL URLWithString:self.configuration.destination.endpoint];
-//    if (nil == url) {
-//        return NO;
-//    }
-//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-//
-//    [request setHTTPMethod:@"POST"];
-//    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-//    [request setValue:self.configuration.destination.accessToken forHTTPHeaderField:@"X-Rollbar-Access-Token"];
-//    [request setHTTPBody:payload];
-//
-//    if (YES == self.configuration.developerOptions.logPayload) {
-//        NSString *payloadString = [[NSString alloc]initWithData:payload
-//                                                       encoding:NSUTF8StringEncoding
-//        ];
-//        NSLog(@"%@", payloadString);
-//        //TODO: if self.configuration.logPayloadFile is defined, save the payload into the file...
-//    }
-//
-//    if (NO == self.configuration.developerOptions.transmit) {
-//        return YES; // we just successfully short-circuit here...
-//    }
-//
-//    __block BOOL result = NO;
-//
-//    // This requires iOS 7.0+
-//    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
-//
-//    NSURLSession *session = [NSURLSession sharedSession];
-//
-//    if (self.configuration.httpProxy.enabled
-//        || self.configuration.httpsProxy.enabled) {
-//
-//        NSDictionary *connectionProxyDictionary =
-//        @{
-//            @"HTTPEnable"   : [NSNumber numberWithInt:self.configuration.httpProxy.enabled],
-//            @"HTTPProxy"    : self.configuration.httpProxy.proxyUrl,
-//            @"HTTPPort"     : @(self.configuration.httpProxy.proxyPort),
-//            @"HTTPSEnable"  : [NSNumber numberWithInt:self.configuration.httpsProxy.enabled],
-//            @"HTTPSProxy"   : self.configuration.httpsProxy.proxyUrl,
-//            @"HTTPSPort"    : @(self.configuration.httpsProxy.proxyPort)
-//        };
-//
-//        NSURLSessionConfiguration *sessionConfig =
-//        [NSURLSessionConfiguration ephemeralSessionConfiguration];
-//        sessionConfig.connectionProxyDictionary = connectionProxyDictionary;
-//        session = [NSURLSession sessionWithConfiguration:sessionConfig];
-//    }
-//
-//    NSURLSessionDataTask *dataTask =
-//    [session dataTaskWithRequest:request
-//               completionHandler:^(
-//                                   NSData * _Nullable data,
-//                                   NSURLResponse * _Nullable response,
-//                                   NSError * _Nullable error) {
-//                                       result = [self checkPayloadResponse:response
-//                                                                     error:error
-//                                                                      data:data];
-//                                       dispatch_semaphore_signal(sem);
-//                                   }];
-//    [dataTask resume];
-//
-//    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
-//
-//    return result;
-}
-
-- (BOOL)checkPayloadResponse:(NSURLResponse *)response
-                       error:(NSError *)error
-                        data:(NSData *)data {
-    
-//    if (NO == self.configuration.developerOptions.suppressSdkInfoLogging) {
-//        
-//        RollbarSdkLog(@"HTTP response from Rollbar: %@", response);
-//    }
-    
-    // Lookup rate limiting headers and adjust reporting rate accordingly:
-    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    NSDictionary *httpHeaders = [httpResponse allHeaderFields];
-    //int rateLimit = [[httpHeaders valueForKey:RESPONSE_HEADER_RATE_LIMIT] intValue];
-    int rateLimitLeft =
-    [[httpHeaders valueForKey:RESPONSE_HEADER_REMAINING_COUNT] intValue];
-    int rateLimitSeconds =
-    [[httpHeaders valueForKey:RESPONSE_HEADER_REMAINING_SECONDS] intValue];
-    if (rateLimitLeft > 0) {
-        
-        self->_nextSendTime = [[NSDate alloc] init];
-    }
-    else {
-        
-        self->_nextSendTime =
-        [[NSDate alloc] initWithTimeIntervalSinceNow:rateLimitSeconds];
-    }
-    
-    if (error) {
-        RollbarSdkLog(@"There was an error reporting to Rollbar");
-        RollbarSdkLog(@"Error: %@", [error localizedDescription]);
-    } else {
-        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
-        if ([httpResponse statusCode] == 200) {
-            
-//            if (NO == self.configuration.developerOptions.suppressSdkInfoLogging) {
-//
-//                RollbarSdkLog(@"Success");
-//            }
-            return YES;
-        } else {
-
-            RollbarSdkLog(@"There was a problem reporting to Rollbar");
-            if (data) {
-                RollbarSdkLog(
-                              @"Response: %@",
-                              [NSJSONSerialization JSONObjectWithData:data
-                                                              options:0
-                                                                error:nil]);
-            }
-        }
-    }
     return NO;
 }
 
