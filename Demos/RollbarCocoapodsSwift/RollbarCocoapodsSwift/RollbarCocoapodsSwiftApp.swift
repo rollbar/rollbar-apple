285bdf95372c45313994d53f61bc0f30df2dabce
Matias Pequeno <matias.pequeno@gmail.com>
2023-06-21 13:29:06 -0300

    WIP demo integrating Rollbar via Cocoapods

---
+++ Demos/RollbarCocoapodsSwift/RollbarCocoapodsSwift/RollbarCocoapodsSwiftApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+//import RollbarCommon
+//import RollbarCrashReport
+import RollbarNotifier
+//import RollbarDeploys
+//import RollbarCocoaLumberjack
+
+@main
+struct RollbarCocoapodsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+    func uncaught() {
+        uncaughtExceptionHandler(NSException())
+    }
+}
