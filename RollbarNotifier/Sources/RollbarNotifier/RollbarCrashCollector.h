7430bfb36013b37f901112f85b80348c6dd68df7
Matias Pequeno <matias.pequeno@gmail.com>
2023-06-30 21:30:55 +0200

    Working SDK with KSCrash integrated as source as-is. (#299)

--- RollbarNotifier/Sources/RollbarNotifier/RollbarCrashCollector.h
+++ RollbarNotifier/Sources/RollbarNotifier/RollbarCrashCollector.h
@@ -1,14 +1,9 @@
 #ifndef RollbarCrashCollector_h
 #define RollbarCrashCollector_h
 
-#if __has_include("KSCrash-umbrella.h")
-@import KSCrash;
-#else
-@import KSCrash_Installations;
-#endif
-
 @import Foundation;
 @import RollbarCommon;
+@import RollbarCrash;
 
 NS_ASSUME_NONNULL_BEGIN
 
