8d3b0e46a342253606c4fb1533f91168399f2e35
Matias Pequeno <matias.pequeno@gmail.com>
2023-07-20 17:23:49 -0300

    Example integrating Rollbar to an App via SPM

---
+++ Examples/Integration/SPMApp/SPMApp/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import RollbarNotifier
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            Rollbar.infoMessage("ContentView Appeared")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
