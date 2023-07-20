b6a5b1caceaf8add14466d212198c9040ae8dbfb
Matias Pequeno <matias.pequeno@gmail.com>
2023-07-20 23:40:19 +0200

    Example integrating Rollbar to a Framework via SPM (#309)

---
+++ Examples/Integration/SPMFramework/SPMFramework/SPMFramework.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMFramework.
+FOUNDATION_EXPORT double SPMFrameworkVersionNumber;
+
+//! Project version string for SPMFramework.
+FOUNDATION_EXPORT const unsigned char SPMFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMFramework/PublicHeader.h>
+
+
