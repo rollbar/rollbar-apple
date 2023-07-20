93e08a0fda16a54823920d59de80c4444427c181
Matias Pequeno <matias.pequeno@gmail.com>
2023-07-20 19:26:04 +0200

    Renamed iosAppSwift to RollbarDemo, moved into new Examples dir (#306)

--- Examples/RollbarDemo/RollbarDemo/RollbarDemoApp.swift
+++ Examples/RollbarDemo/RollbarDemo/RollbarDemoApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import RollbarNotifier
 
 @main
-struct iosAppSwiftApp: App {
+struct RollbarDemoApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
